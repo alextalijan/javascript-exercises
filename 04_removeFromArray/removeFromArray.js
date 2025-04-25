612ed0783f69f7f9fc6482c05ce3b2fd5df1a9ee
alextalijan <acatalijan@icloud.com>
2025-04-25 15:28:01 -0500

    feat: write function to remove members from array
    Passes all unit tests and successfully removes
    all instances of provided elements from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...membersToRemove) {
+    for (const member of membersToRemove) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === member) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
