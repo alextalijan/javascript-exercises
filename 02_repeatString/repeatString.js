9cf0f0ea6e3e38ec92079374400b7bed4263062c
alextalijan <acatalijan@icloud.com>
2025-04-25 14:46:02 -0500

    feat: write the whole logic for repeating strings
    The function passes all of the unit tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepetitions) {
+    if (numberOfRepetitions < 0) {
+        return "ERROR";
+    }
 
+    let repeatedString = '';
+
+    for (let i = 0; i < numberOfRepetitions; i++) {
+        repeatedString = repeatedString.concat(string);
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
