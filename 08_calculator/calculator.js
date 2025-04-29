fe5795e997c2f6cd75abb9c4fcf9be5dc7bd92b0
alextalijan <acatalijan@icloud.com>
2025-04-29 18:35:02 -0500

    feat: write logic for all calculator functions
    All the unit tests pass.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+    return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+    let product = 1;
 
+    for (let i = 0; i < numbers.length; i++) {
+        product *= numbers[i];
+    }
+
+    return product;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+    let result = 1;
+
+    for (let i = 0; i < power; i++) {
+        result *= number;
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+    let result = 1;
+
+    for (let i = 1; i <= number; i++) {
+        result *= i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
