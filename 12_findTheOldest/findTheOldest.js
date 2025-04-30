4dfea25452b4288d5de5959a0ada0942b76c8d64
alextalijan <acatalijan@icloud.com>
2025-04-29 19:52:47 -0500

    feat: write logic for determining oldest person
    From the list of people objects, return the one with the oldest age.
    Passes all the unit tests.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Sort the people from oldest to youngest and return the first one
+    people.sort((firstPerson, nextPerson) => {
+        // If the year of death is undefined, use the current year
+        if (firstPerson.yearOfDeath === undefined) {
+            firstPerson.yearOfDeath = new Date().getFullYear();
+        }
+        if (nextPerson.yearOfDeath === undefined) {
+            nextPerson.yearOfDeath = new Date().getFullYear();
+        }
+            
+        return nextPerson.yearOfDeath - nextPerson.yearOfBirth -
+        (firstPerson.yearOfDeath - firstPerson.yearOfBirth);
+    });
 
+    return people[0];
 };
 
 // Do not edit below this line
