f6f0bc3938597705bbd049afd6d7d427749076ea
alextalijan <acatalijan@icloud.com>
2025-04-29 19:35:47 -0500

    feat: add logic that returns n-th fibonacci number
    Passes all of the unit tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(numberIndex) {
+    // If the index is negative, abort the function
+    if (numberIndex < 0) {
+        return "OOPS";
+    }
 
+    const fibonacci = [0, 1]
+
+    // Generate a fibonacci sequence of proper length and return the last element
+    for (let i = 2; i <= numberIndex; i++) {
+        fibonacci.push(fibonacci[i - 2] + fibonacci[i - 1]);
+    }
+
+    return fibonacci[numberIndex];
 };
 
 // Do not edit below this line
